f538a865ce20f308b7d54c16c761615e97e0d355
Aleksandr Chernikov <58355477+AlexChernikov@users.noreply.github.com>
2020-10-21 19:57:38 +0300

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,30 @@
+function validate(){
+  var username = document.getElementById("login").value;
+  var password = document.getElementById("password").value;
+  $.getJSON("logins.json", function(json) {
+    $.each(json, function(i, user) {
+      console.log(user);
+      if(username === user.login && password === user.password) {
+        alert ("Login successfully. Welcome, " + user.name);
+        closeForm()
+      }
+    });
+  });
+}
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+}
+
+function openForm() {
+  document.getElementById('id01').style.display='block'
+}
+
+function closeForm() {
+  document.getElementById("id01").style.display = "none";
+}
