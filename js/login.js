28eb355952ac5e0e364ec8798dd558468f926cb2
Alexandr <sda4000@gmail.com>
2020-10-22 00:21:49 +0300

    Added login functional. Added login error.

--- js/login.js
+++ js/login.js
@@ -1,29 +1,46 @@
-function validate(){
-  var username = document.getElementById("login").value;
-  var password = document.getElementById("password").value;
-  $.getJSON("logins.json", function(json) {
-    $.each(json, function(i, user) {
-      if(username === user.login && password === user.password) {
-        alert ("Здравствуйте, " + user.firstName);
-        closeForm()
-      }
+function validate() {
+    var username = document.getElementById("login").value;
+    var password = document.getElementById("password").value;
+    $.getJSON("logins.json", function (json) {
+        var incorrect = true;
+        $.each(json, function (i, user) {
+            if (username === user.login && password === user.password) {
+                alert("Здравствуйте, " + user.firstName);
+                document.getElementById("lb").innerText = user.firstName + "_" + user.lastName;
+                incorrect = false;
+                closeForm()
+                entered = true;
+                document.getElementById("ct").style.display = 'inline';
+                document.getElementById("ct1").style.display = 'inline';
+                document.getElementById("contacts").style.display = 'inline';
+            }
+        });
+        if (incorrect) {
+            alert("Ошибка входа, проверьте данные!")
+        }
     });
-  });
-}
-// Get the modal
-var modal = document.getElementById('id01');
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target === modal) {
-    modal.style.display = "none";
-  }
+
+}
+
+function checkLogin() {
+    if (!entered) {
+        slowScroll('#top')
+        openForm();
+    } else {
+        document.getElementById("lb").innerText = "Вход";
+        document.getElementById("ct").style.display = 'none';
+        document.getElementById("ct1").style.display = 'none';
+        document.getElementById("contacts").style.display = 'none';
+        entered = false;
+        slowScroll('#top')
+    }
 }
 
 function openForm() {
-  document.getElementById('id01').style.display='block'
+    document.getElementById('id01').style.display = 'block'
 }
 
 function closeForm() {
-  document.getElementById("id01").style.display = "none";
+    document.getElementById("id01").style.display = "none";
 }
